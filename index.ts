a5a96a0dae6596fc1f23ce7eedbd35aabeb42857
Albéric Cohen <acn@edifixio.com>
2020-03-30 11:26:48 +0200

    fix(ajouut depuis branch2)

--- index.ts
+++ index.ts
@@ -2,4 +2,5 @@ const world = 'World';
 console.log('Hello' + world)
 console.log('je teste git')
 console.log('ajoutée depuis master')
+console.log('jajoute depuis branch2')
 console.log('pwetpwet')
